3df166b011be645a63812dfe7845df5cf9ef1627
KYLIBISHOPC256\kylinmb <kyli.nmb@gmail.com>
2020-04-07 14:56:41 -0600

    Organized code a bit better and added some comments

---
+++ src/GetUserClimbData.py
@@ -0,0 +1,51 @@
+from src import RequestMPData as mp
+import pandas as pd
+
+
+def get_users_climbs(path_to_user_ids):
+    """
+    Creates data frame where rows are users and columns are climbs.
+    Entries represent a user's rating for a climb.
+    If a user has completed a climb, but not given it a rating the minimum rating is assumed.
+    If a user has completed a climb and given it a rating their rating is used.
+    A zero indicates that a user has not completed that climb
+    :param path_to_user_ids: path to list of user_ids
+    :return: DataFrame users x climbs
+    """
+    user_ids = pd.read_csv(path_to_user_ids, sep='\n', header=None)
+    users_climbs = pd.DataFrame()
+    for index, user_id in user_ids.iterrows():
+        single_user_climb = pd.DataFrame()
+        single_user_climb['user_id'] = user_id
+        users_ticks = mp.get_ticks(user_id[0])['ticks']
+        for t in users_ticks:
+            route_id = t['routeId']
+            user_stars = t['userStars']
+            single_user_climb[route_id] = user_stars if not user_stars == -1 else 1
+        users_climbs = pd.concat([users_climbs, single_user_climb], sort=False, ignore_index=True)
+    return users_climbs.fillna(0)
+
+
+def get_users_climbs_binary(path_to_user_ids):
+    """
+    Creates data frame where rows are users and columns are climbs.
+    Entries represent if a user has completed a climb.
+    A one indicates that a user has completed that climb.
+    A zero indicates that a user has not completed that climb
+    :param path_to_user_ids: path to list of user_ids
+    :return: DataFrame users x climbs
+    """
+    user_ids = pd.read_csv(path_to_user_ids, sep='\n', header=None)
+    users_climbs = pd.DataFrame()
+    for index, user_id in user_ids.iterrows():
+        single_user_climb = pd.DataFrame()
+        single_user_climb['user_id'] = user_id
+        users_ticks = mp.get_ticks(user_id[0])['ticks']
+        for t in users_ticks:
+            route_id = t['routeId']
+            single_user_climb[route_id] = 1
+        users_climbs = pd.concat([users_climbs, single_user_climb], sort=False, ignore_index=True)
+    return users_climbs.fillna(0)
+
+
+test = get_users_climbs_binary(path_to_user_ids='user_ids/ExampleID.csv')
--- src/RequestMPData.py
+++ src/RequestMPData.py
@@ -1,3 +1,8 @@
+"""
+This file contains functions to connect to all the mountain project
+end point. The mountain project API is explained here:
+https://www.mountainproject.com/data/
+"""
 import json
 import requests
 from requests.exceptions import RequestException
@@ -7,6 +12,11 @@ key = '<SET YOUR KEY HERE>'
 
 
 def get_user(user_id):
+    """
+    Requests user data from mountain project
+    :param user_id: the user's id
+    :return: dictionary of users information
+    """
     full_url = base_url + 'get-user?userId=' + user_id + '&key=' + key
     response = requests.get(full_url)
     if response.status_code != 200:
@@ -15,6 +25,11 @@ def get_user(user_id):
 
 
 def get_ticks(user_id):
+    """
+    Requests user's ticks from mountain project
+    :param user_id:  The user's id
+    :return: dictionary of user's ticks
+    """
     full_url = base_url + 'get-ticks?userId=' + str(user_id) + '&key=' + key
     response = requests.get(full_url)
     if response.status_code != 200:
@@ -23,6 +38,11 @@ def get_ticks(user_id):
 
 
 def get_todos(user_id):
+    """
+    Returns user's to do's
+    :param user_id: The user's id
+    :return: dictionary of user's to do's
+    """
     full_url = base_url + 'get-to-dos?userId=' + user_id + '&key=' + key
     response = requests.get(full_url)
     if response.status_code != 200:
@@ -31,7 +51,12 @@ def get_todos(user_id):
 
 
 def get_routes(route_ids):
-    route_ids = ','.join(route_ids)
+    """
+    Returns information on routes
+    :param route_ids: List of route id's
+    :return: dictionary of route information
+    """
+    route_ids = ','.join(str(r) for r in route_ids)
     full_url = base_url + 'get-routes?routeIds=' + route_ids + '&key=' + key
     response = requests.get(full_url)
     if response.status_code != 200:
